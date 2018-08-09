ea662dc1e4138afa44706fe5328eea2171d833e1
LegendHuziqi <948198126@qq.com>
2018-08-09 08:22:26 +0800

    first commit

---
+++ src/org/Pojo/Node.java
@@ -0,0 +1,68 @@
+package org.Pojo;
+
+public class Node {
+    private Long nodeId;
+    private String nodeName;
+    private String nodeLocation;
+    private Integer nodeAbility;
+    private Integer nodeRAM;
+    private Integer nodeROM;
+    private String IP;
+
+    public Long getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(Long nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getNodeLocation() {
+        return nodeLocation;
+    }
+
+    public void setNodeLocation(String nodeLocation) {
+        this.nodeLocation = nodeLocation;
+    }
+
+    public Integer getNodeAbility() {
+        return nodeAbility;
+    }
+
+    public void setNodeAbility(Integer nodeAbility) {
+        this.nodeAbility = nodeAbility;
+    }
+
+    public Integer getNodeRAM() {
+        return nodeRAM;
+    }
+
+    public void setNodeRAM(Integer nodeRAM) {
+        this.nodeRAM = nodeRAM;
+    }
+
+    public Integer getNodeROM() {
+        return nodeROM;
+    }
+
+    public void setNodeROM(Integer nodeROM) {
+        this.nodeROM = nodeROM;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+}
