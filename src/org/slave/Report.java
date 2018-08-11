c73e56ba56f2c8b8737f32c5cbcadc6f2d1d2122
LegendHuziqi <948198126@qq.com>
2018-08-11 10:14:04 +0800

    add tow jar file(sigar and fastjson)

--- src/org/slave/Report.java
+++ src/org/slave/Report.java
@@ -2,10 +2,19 @@ package org.slave;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import org.hyperic.sigar.*;
+import com.alibaba.fastjson.*;
+
+import static com.alibaba.fastjson.JSON.toJSONString;
 
 public class Report {
     public static String reportCPU() {
+        class CPU{
+            Integer core;
+            Integer frequency;
+        }
         try {
+            CPU cpu=new CPU();
             String shpath = "/home/legendhu/IdeaProjects/Serverless/testCPU.sh";
             Process ps = Runtime.getRuntime().exec(shpath);
             ps.waitFor();
@@ -17,18 +26,24 @@ public class Report {
                 sb.append(line);
             }
             String result = sb.toString();
-            String core=result.substring(6,7);
+            cpu.core=Integer.parseInt(result.substring(6,7));
             String tmp=result.substring(result.length()-7,result.length()-3);
             Float tmp2=Float.parseFloat(tmp)*1000;
-            Integer frequency=(int)tmp2.floatValue();
+            cpu.frequency=(int)tmp2.floatValue();
+            result=toJSONString(cpu);
             System.out.println(result);
-            System.out.println(core);
-            System.out.println(frequency);
             return result;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static String reportRAM() throws SigarException {
+        Sigar sigar = new Sigar();
+        Mem mem = sigar.getMem();
+        System.out.println(mem.getTotal());
+        return null;
     }
 
 //    public static String reportRAM(){
